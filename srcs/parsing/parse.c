3c1eaf1d2128e6f99ca8f4aaeea72ebe217922c8
Spawnzer <78969151+Spawnzer@users.noreply.github.com>
2022-05-10 13:48:30 -0400

    Update parse.c

--- srcs/parsing/parse.c
+++ srcs/parsing/parse.c
@@ -15,6 +15,19 @@
 
 void	scan_builtins(char **options);
 
+void ms_freeList(struct node* head)
+{
+	struct node* tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		free(tmp->value);
+		head = head->next;
+		free(tmp);
+	}
+}
+
 char	get_type(char *str, char *sym)
 {
 	char a = sym[0];
@@ -59,8 +72,6 @@ void	ft_to_do(t_node *head, char *sym)
 	}
 }
 
-
-
 int ms_parsing(void)
 {
 	t_minishell *minishell;
@@ -68,18 +79,23 @@ int ms_parsing(void)
 	t_node *tmp;
 	char sym[4] = {'|', '<', '"', '\''}; 
 	minishell = get_minishell();
-	char **arg = ms_split(get_var(minishell->user_input, 1, -1), '|');
-	int i = 1;
-	t_node *head = new_node(arg[0], sym);
-	while(arg[i] != NULL)
+	if (minishell->user_input[0] != '\0' && ms_sanitize(minishell->user_input))
 	{
-		list = new_node(arg[i], sym);
-		add_at_end(&head, list);
-		i++;
+		char **arg = ms_split(get_var(minishell->user_input, 1, -1), '|');
+		if (ft_strlen(arg[0]) == 0)
+			return 0;
+		int i = 1;
+		minishell->head = new_node(arg[0], sym);
+		while (arg[i] != NULL) {
+			list = new_node(arg[i], sym);
+			add_at_end(&minishell->head, list);
+			i++;
+		}
+		free(arg);
+		tmp = minishell->head;
+		//printlist(tmp);
+		ft_to_do(tmp, sym);
 	}
-	tmp = head;
-	//printlist(tmp);
-	ft_to_do(tmp, sym);
 	//printf ("%s\n", av[1]);
 	return 0;	
 }
