1e8c928ccd24e300ad5aa92cdd0a25bec810e21f
Spawnzer <78969151+Spawnzer@users.noreply.github.com>
2022-05-19 11:40:39 -0400

    Update nodes.c

--- srcs/parsing/nodes.c
+++ srcs/parsing/nodes.c
@@ -1,34 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parse.c                                            :+:      :+:    :+:   */
+/*   nodes.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adubeau <marvin@42quebec.com>              +#+  +:+       +#+        */
+/*   By: tshimoda <tshimoda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 16:42:54 by adubeau           #+#    #+#             */
-/*   Updated: 2022/04/30 16:34:26 by adubeau          ###   ########.fr       */
+/*   Updated: 2022/05/18 17:02:31 by adubeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 #include <string.h>
 
-char *ms_strip(char *str, int i, int j)
+char	*ms_strip(char *str, int i, int j)
 {
-	char *tmp = ft_calloc(ft_strlen(str), sizeof(char));
+	char	*tmp;
 
+	tmp = ft_calloc(ft_strlen(str), sizeof(char));
 	if (!str)
-		return NULL;
-	//check_qm(str, -1, 1, 1);
+		return (NULL);
 	if (!(ft_isprint(str[i])))
 		i++;
-	printf("i = %d, 1st char = %d\n", i, (int)str[i]);
 	while (str[i])
 	{
-		/*if (s == 0 && str[i] == '\'')
-			i++;
-		else if (d == 0 && str[i] == '"')
-			i++;*/
 		if (str[i] == '\'')
 		{
 			while (str[++i] && str[i] != '\'')
@@ -42,48 +37,47 @@ char *ms_strip(char *str, int i, int j)
 		else
 			tmp[j++] = str[i++];
 	}
-	if (str)
-		free(str);
+	free(str);
 	return (tmp);
 }
 
-t_node	*new_node(char *str, char *sym)
+t_node	*new_node(char *str)
 {
-	t_node	*new = malloc(sizeof(t_node));
-	new->value = ms_strip(str, 0, 0);
-	new->type = get_type(str, sym);
-	new->fdI = get_fdI(new, 0, 0);
-	new->fdO = get_fdO(new, 0, 0, 1);
-	new->pipe_end = NULL;
-	new->next = NULL;
-	return new;
-}
+	t_node	*new;
 
-void	printlist(t_node *head)
-{
-	t_node *tmp = head;
-
-	while (tmp != NULL)
+	new = malloc(sizeof(t_node));
+	if (str != NULL)
 	{
-		printf("Value:%s\n", tmp->value);
-		printf("Type:%c\n", tmp->type);
-		tmp = tmp->next;
+		if (ft_is_present('\'', str) || ft_is_present('\"', str))
+			new->value = ms_strip(str, 0, 0);
+		else
+			new->value = str;
+		new->eof = NULL;
+		new->type = get_type(str);
+		new->fd_i = get_fd_i(new, new->value, 0, 0, 0);
+		new->fd_o = get_fd_o(new, new->value, 0, 0, 1);
+		new->id = -1;
+		new->next = NULL;
+		new->prev = NULL;
 	}
+	return (new);
 }
 
 t_node	*add_at_head(t_node **head, t_node *new)
 {
 	new->next = *head;
 	*head = new;
-	return new;
+	return (new);
 }
 
 void	*add_at_end(t_node **head, t_node *new)
 {
-	t_node *tmp = *head;
+	t_node	*tmp;
 
+	tmp = *head;
 	while (tmp->next != NULL)
 		tmp = tmp->next;
+	new->prev = tmp;
 	tmp->next = new;
-	return 0;
+	return (0);
 }
