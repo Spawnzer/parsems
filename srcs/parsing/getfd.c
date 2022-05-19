697555e53f6d68c24ffa5faf601b4c16de6f23bd
Spawnzer <78969151+Spawnzer@users.noreply.github.com>
2022-05-19 11:47:27 -0400

    Update getfd.c

--- srcs/parsing/getfd.c
+++ srcs/parsing/getfd.c
@@ -1,78 +1,106 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parse.c                                            :+:      :+:    :+:   */
+/*   getfd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adubeau <marvin@42quebec.com>              +#+  +:+       +#+        */
+/*   By: tshimoda <tshimoda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 16:42:54 by adubeau           #+#    #+#             */
-/*   Updated: 2022/04/30 16:34:26 by adubeau          ###   ########.fr       */
+/*   Updated: 2022/05/18 17:25:29 by adubeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 #include <string.h>
 
-int		get_fdI(t_node *current, int i, int j)
+int	get_fd_i(t_node *current, char *value, int i, int j, int fd)
 {
+	int		k;
+	char	*file;
+	char	*tmp;
 
-	int fd;
-	char *file;
-
-	fd = 0;
-	while (current->value[i])
+	while (value[i])
 	{
-		if (current->value[i] == ('<'))
-			if (current->value[i + 1] != '<')
-			{
-				while (!(ft_isalnum(current->value[i])))
-					i++;
-				while (ft_isalnum(current->value[i++]))
-					j++;
-				file = ft_substr(current->value, i-j, j);
-				fd = open(file, O_RDONLY);
-			}
-		i++;
-	}
-	return (fd);
-}
-int	get_fdO(t_node *current, int i, int j, int fd)
-{
-	int k;
-	char *file;
-	char *tmp;
-
-	while (current->value[i])
-	{
-		if (current->value[i] == '>')
+		j = 0;
+		if (value[i] == '<')
 		{
-			k = i - 1;
-			while (current->value[i] == ' ' || current->value[i] == '>')
+			k = i;
+			while (value[i] == ' ' || value[i] == '<')
 				i++;
-			while (current->value[i] && current->value[i] != ' ' && current->value[i] != '>')
+			while (value[i] && value[i] != ' ' && value[i] != '<')
 			{
 				j++;
 				i++;
 			}
-			file = ft_substr(current->value, i - j, j);
-			if (ft_is_present('/', file))
+			file = ft_substr(value, i - j, j);
+			fd = open(file, O_RDONLY);
+			if (fd == -1)
 			{
-				current->value = "";
+				current->type = 'e';
 				printf("%s: No such file or directory\n", file);
-				return (1);
+				break ;
 			}
-			printf("\nfile:%s\n", file);
-			tmp = ft_strjoin(ft_substr(current->value, 0, k + 1), (current->value + i));
-			free(current->value);
-			current->value = tmp;
-			free(tmp);
-			if (current->type == 'c')
-				fd = open(file, O_RDWR | O_CREAT | O_TRUNC, 0777);
-			else
-				fd = open(file, O_RDWR | O_CREAT | O_APPEND, 0777);
-			return (get_fdO(current, 0, 0, fd));
+			tmp = ft_strjoin(ft_substr(value, 0, k), (value + i));
+			free(value);
+			value = tmp;
+			tmp = NULL;
+			i = -1;
 		}
 		i++;
 	}
+	current->value = value;
+	return (fd);
+}
+
+int	get_fd_o(t_node *current, char *value, int i, int j, int fd)
+{
+	int		k;
+	char	*file;
+	char	*tmp;
+	char	type;
+
+	while (value[i])
+	{
+		j = 0;
+		if (value[i] == '>')
+		{
+			k = i - 1;
+			if (value[i + 1] == '>')
+			{
+				current->type = 'a';
+				i++;
+			}
+			else
+				type = 'c';
+			while (value[i] == ' ' || value[i] == '>')
+				i++;
+			while (value[i] && value[i] != ' ' && value[i] != '>')
+			{
+				j++;
+				i++;
+			}
+			file = ft_substr(value, i - j, j);
+			if (ft_is_present('/', file))
+				printf("minishell: %s: No such file or directory\n", file);
+			tmp = ft_strjoin(ft_substr(value, 0, k), (value + i));
+			free(value);
+			value = tmp;
+			tmp = NULL;
+			printf("file = %s, value = %s, type = %c\n", file, value, current->type);
+			if (type == 'c')
+			{
+				fd = open(file, O_RDWR | O_CREAT | O_TRUNC, 0777);
+				//free(file);
+			}
+			else
+			{
+				fd = open(file, O_RDWR | O_CREAT | O_APPEND, 0777);
+				//free(file);
+			}
+			i = -1;
+		}
+		i++;
+	}
+	current->value = value;
 	return (fd);
 }
