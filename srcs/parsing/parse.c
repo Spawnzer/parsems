cbc6e7f886eae9d76fcdd7dd9339354523fa3265
Spawnzer <78969151+Spawnzer@users.noreply.github.com>
2022-05-19 11:39:42 -0400

    Update parse.c

--- srcs/parsing/parse.c
+++ srcs/parsing/parse.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adubeau <marvin@42quebec.com>              +#+  +:+       +#+        */
+/*   By: tshimoda <tshimoda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 16:42:54 by adubeau           #+#    #+#             */
-/*   Updated: 2022/04/30 16:34:26 by adubeau          ###   ########.fr       */
+/*   Updated: 2022/05/18 17:24:22 by adubeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 
 void	scan_builtins(char **options);
 
-void ms_freeList(struct node* head)
+void	ms_free_list(struct node *head)
 {
-	struct node* tmp;
+	struct node	*tmp;
 
 	while (head != NULL)
 	{
@@ -28,74 +28,62 @@ void ms_freeList(struct node* head)
 	}
 }
 
-char	get_type(char *str, char *sym)
+char	get_type(char *str)
 {
-	char a = sym[0];
-	a = 'b';
 	if (ft_is_present('<', str))
 	{
 		if (str[ft_is_present('<', str) + 1] == '<')
-			return 'h';
+			return ('h');
 	}
 	else if (ft_is_present('>', str))
 	{
-			if (str[ft_is_present('>', str) + 1] == '>')
-				return 'a';
+		if (str[ft_is_present('>', str) + 1] == '>')
+			return ('a');
 	}
-	else if (str[0] == (char)NULL)
-			return 'e';
-	return 'c';
+	else if (str[0] == (char) NULL)
+		return ('e');
+	return ('c');
 }
+//minishell> echo "$a" $a  '$a' $a
+//		p p '$a' p
 
-void	ft_to_do(t_node *head, char *sym)
+//<txt <txt3 <txt7 >txt4 >txt2 >txt7 < txt3 cat <txt > text
+//malloc error
+
+//<sdfgdsfg
+//ok
+//<sdfgdsfg <sdfgdsfgdfg
+//malloc error
+
+int	ms_parsing(void)
 {
-	t_node *tmp1 = head;
+	t_minishell	*minishell;
+	t_node		*list;
+	t_node		*tmp;
+	char		**arg;
+	int			i;
 
-	if (sym[0] == '!')
-		printf("Hi");
-	while (tmp1 != NULL)
-	{
-		//tmp1->type = get_type(tmp1->value, sym);
-		printf("value:%s, type:%c, fdI:%d, fdO:%d\n", tmp1->value, tmp1->type, tmp1->fdI, tmp1->fdO);
-		if (tmp1->type == 'c' || tmp1->type == 'a')
-			scan_builtins(ft_split(tmp1->value, ' '));
-		else if (tmp1->type == 'p')
-				printf("and pipe it into\n");
-		else if (tmp1->type == 'r')
-			printf("and redirect it into\n");
-		else if (tmp1->type == 'h')
-		{
-			printf("do heredoc bs with %s\n", tmp1->next->value);
-			tmp1 = tmp1->next;
-		}
-		tmp1 = tmp1->next;
-	}
-}
-
-int ms_parsing(void)
-{
-	t_minishell *minishell;
-	t_node *list;
-	t_node *tmp;
-	char sym[4] = {'|', '<', '"', '\''}; 
 	minishell = get_minishell();
 	if (minishell->user_input[0] != '\0' && ms_sanitize(minishell->user_input))
 	{
-		char **arg = ms_split(get_var(minishell->user_input, 1, -1), '|');
+		arg = ms_split(get_var(minishell->user_input, 1, -1), '|');
+		minishell->head = new_node(arg[0]);
 		if (ft_strlen(arg[0]) == 0)
-			return 0;
-		int i = 1;
-		minishell->head = new_node(arg[0], sym);
-		while (arg[i] != NULL) {
-			list = new_node(arg[i], sym);
+			return (0);
+		i = 1;
+		while (arg[i] != NULL)
+		{
+			list = new_node(arg[i]);
 			add_at_end(&minishell->head, list);
 			i++;
 		}
-		free(arg);
 		tmp = minishell->head;
-		//printlist(tmp);
-		ft_to_do(tmp, sym);
+		while (tmp != NULL)
+		{
+			printf("value:%s,type:%c,fdi:%d,fdo:%d\n", tmp->value, tmp->type, tmp->fd_i, tmp->fd_o);
+			tmp = tmp->next;
+		}
+		return (1);
 	}
-	//printf ("%s\n", av[1]);
-	return 0;	
+	return (0);
 }
