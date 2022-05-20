348827d3761c2c3cd4341f8e8cb8804ed139f8d4
Spawnzer <78969151+Spawnzer@users.noreply.github.com>
2022-05-19 20:32:48 -0400

    Update parsing.h

--- incls/parsing.h
+++ incls/parsing.h
@@ -6,15 +6,15 @@
 /*   By: tshimoda <tshimoda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 16:50:49 by adubeau           #+#    #+#             */
-/*   Updated: 2022/05/18 17:32:27 by adubeau          ###   ########.fr       */
+/*   Updated: 2022/05/19 11:52:23 by tshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-# include "builtins.h"
+
 
 #ifndef PARSING_H
 # define PARSING_H
 
-struct node 
+typedef struct s_node 
 {
 	char		*value;
 	char		*eof;
@@ -23,25 +23,20 @@ struct node
 	int			fd_o;
 	pid_t		id;
 	int			pipe_end[2];
-	struct node	*next;
-	struct node	*prev;
-};
+	struct s_node	*next;
+	struct s_node	*prev;
+}t_node;
 
-typedef struct node	t_node;
-
-void		ms_free_list(struct node *head);
+void		ms_free_list(t_node *head);
 int			ms_sanitize(char *input);
-char		check_qm(char *str, int i, int s, int d);
 int			get_fd_i(t_node *current, char *value, int i, int j, int fd);
 int			get_fd_o(t_node *current, char *value, int i, int j, int fd);
 char		*get_var(char *str, int quote, int i);
-t_node		*add_at_head(t_node **head, t_node *new);
 void		*add_at_end(t_node **head, t_node *new);
 char		get_type(char *str);
 t_node		*new_node(char *str);
-void		printlist(t_node *head);
-int			ms_parsing(void);
+int			ms_parsing(int i);
 int			ft_is_present(char c, char *sym);
-char		**ms_split(char const *s, char sym);
+char		**ms_split(char const *s, char sym, unsigned int i, unsigned int len);
 
 #endif
