e7492141fa87339c425e28f63506da8a8f5e21a9
Spawnzer <78969151+Spawnzer@users.noreply.github.com>
2022-06-01 14:35:03 -0400

    Update builtin_utils_env3.c

--- srcs/builtins/builtin_utils_env3.c
+++ srcs/builtins/builtin_utils_env3.c
@@ -1,68 +1,48 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   builtin_utils_env3.c                               :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: tshimoda <tshimoda@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/16 13:13:07 by tshimoda          #+#    #+#             */
-/*   Updated: 2022/04/21 17:18:13 by tshimoda         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "builtins.h"
-
-// function prints the list of the env variable not in alphabetical order
-void	env_var_print(void)
+char *ft_trim(char *str, char c)
 {
-	// CHECK IF ONLY A KEY, DONT PRINT IT!!!
-	t_minishell *minishell;
-	int i;
+	char	*tmp;
+	int 	i;
+	int 	j;
 
 	i = 0;
-	minishell = get_minishell();
-	while (minishell->env[i] != NULL)
+	j = 0;
+	//freeeeeeeeee meeeeeeeeee
+	tmp = ft_calloc(ft_strlen(str), sizeof(char));
+	while (str[i])
 	{
-		if (env_var_is_key_only(minishell->env[i]) == NO)
-		{
-			printf("\033[1;33m print NOT in order i = %d var = %s \033[0m \n", i, minishell->env[i]);
-			// printf("%s\n", minishell->env[i]);
-		}
+		if (str[i] != c)
+			tmp[j++] = str[i];
 		i++;
 	}
+	return (tmp);
 }
 
-void	env_var_print_in_order(t_minishell *ms, int i, int j)
+t_answer	ft_is_a_match(char *keyword, char *input)
 {
-	char **table;
-	
-	table = ft_calloc(ms->env_size, sizeof(char *));
-	while (ms->env[i] != NULL)
-	{
-		printf("print in order segfaults???\n");
-		table[i] = ft_strdup(ms->env[i]);
-		i++;
-	}
-	i = 0;
-	while (ms->env[i] != NULL)
-	{
-		j = i + 1;
-		while (table[j] != NULL)
-		{
+	int		i;
+	int		len;
+	char	*tmp;
 
-			if (ft_strcmp(table[i], table[j]) > 0)
-				ft_swap(&table[i], &table[j]);
-			j++;
-		}
-		i++;
-	}
-	table[i] = NULL;
 	i = 0;
-	while (table[i])
+	len = ft_strlen(input);
+	if (input == NULL)
+		return (NO);
+	tmp = ft_trim(input, ' ');
+	if (ft_strlen(keyword) != ft_strlen(tmp))
 	{
-		printf("\033[1;34m print in order i = %d var = %s \033[0m \n", i, table[i]);
-	//	printf("%s\n", table[i++]);
-		i++;
+		free(tmp);
+		return (NO);
 	}
-	ft_free_table(table);
+	while (i < len)
+	{
+		if (keyword[i] == tmp[i])
+			i++;
+		else
+		{
+			free(tmp);
+			return (NO);
+		}
+	}
+	free(tmp);
+	return (YES);
 }
